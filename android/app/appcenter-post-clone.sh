d69e55fac4cef1897fdb2c722ec18a1a1ee6a242
Manas Shrestha <manas@Manass-MacBook-Pro.local>
2020-08-20 10:04:14 +0545

    Change appcenter build config to release

--- android/app/appcenter-post-clone.sh
+++ android/app/appcenter-post-clone.sh
@@ -16,7 +16,7 @@ export PATH=`pwd`/flutter/bin:$PATH
 
 flutter channel stable
 flutter doctor
-flutter build apk --debug
+flutter build apk --release
 
 # copy the APK where AppCenter will find it
-mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/debug/app-debug.apk $_
+mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/release/app-release.apk $_
